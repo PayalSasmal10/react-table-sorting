b1a17fb7effd19127ed53aa28ae8d5e05485e650
Payal Sasmal <sasmalpayal@gmail.com>
2023-06-22 23:12:53 +0530

    react app table sorting is on progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import JSONDATA from "./JSONDATA.json";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [sortConfig, setSortConfig] = useState(null);
+  const [sortedField, setSortedField] = useState(null);
+  const [items, setItems] = useState([]);
+  // let newName = [];
+
+  // newName = [...newName, ...name.split()];
+  // console.log(newName);
+  // return newName.sort();
+  useEffect(() => {
+    setItems(JSONDATA);
+  })
+
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th onClick={() => setSortConfig("name")}>Name</th>
+            <th>Address</th>
+            <th>GeoLocation</th>
+          </tr>
+        </thead>
+        {items.map((val, id) => {
+          return (
+            <tbody key={id}>
+              <tr>
+                <td>{val.name}</td>
+                <td>{val.address?.city}</td>
+                <td>{val.address?.geo?.lat}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
     </div>
   );
 }
