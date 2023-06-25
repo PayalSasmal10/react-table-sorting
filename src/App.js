4a0618d2f0f9c6d7ba64fc2c68146f556c3dd732
Payal Sasmal <sasmalpayal@gmail.com>
2023-06-25 16:33:37 +0530

    sorting based on name is working

--- src/App.js
+++ src/App.js
@@ -3,41 +3,44 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [ascending, setAscending] = useState(false);
-  const [descending, setDescending] = useState(false);
-  const [sortedField, setSortedField] = useState(null);
-  const [items, setItems] = useState([]);
+  const [sortColumn, setSortColumn] = useState(null);
+  const [sortDirection, setSortDirection] = useState(null);
+  const [items, setItems] = useState(JSONDATA);
   // let newName = [];
 
   // newName = [...newName, ...name.split()];
   // console.log(newName);
   // return newName.sort();
-  useEffect(() => {
-    setItems(JSONDATA);
-  });
+ 
+  const handleSort = (column) => {
+    const newDirection = column === sortColumn && sortDirection === 'asc'? 'desc' : 'asc';
+    
+    const sortedData = [...items].sort((a, b) => {
+        console.log("column", column);
 
-  if (sortedField !== null) {
-    items.sort((a, b) => {
-      console.log("a :", a);
-      console.log("b :", b);
-      if (a.name < b.name)  {
-        return -1;
-      }
-      if (a.name > b.name) {
-        return 1;
-      }
-      return 0;
-    });
-  }
+        if (a?.column < b?.column) {
+          return newDirection === 'asc' ? -1 : 1;
+        }
+        if (a[column] > b[column]) {
+          return newDirection === 'asc' ? 1 : -1;
+        }
+        return 0;
+      });
+    
+    setItems(sortedData);
+    setSortColumn(column);
+    setSortDirection(newDirection);
+  
+  };
 
   return (
     <div className="App">
       <table>
         <thead>
           <tr>
-            <th onClick={() => setSortedField("name")}>Name</th>
-            <th>Address</th>
-            <th>GeoLocation</th>
+            <th onClick={() => handleSort("name")}>Name {sortColumn === 'name' && sortDirection === 'asc' ? '▲' : '▼'}</th>
+            <th onClick={() => handleSort("address")}>Address {sortColumn === 'address' && sortDirection === 'asc' ? '▲' : '▼'}</th>
+            <th onClick={() => handleSort("geo")}>GeoLocation {sortColumn === 'geo' && sortDirection === 'asc' ? '▲' : '▼'}</th>
           </tr>
         </thead>
         {items.map((val, id) => {
